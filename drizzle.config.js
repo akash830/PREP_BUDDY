fc5eb85b491dedc3c92fe4fbef296b32cbf762a2
Akash Raj P <70941042+akash830@users.noreply.github.com>
2026-05-07 00:41:57 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.js", // Path to your schema file
     dialect: "postgresql",
     dbCredentials: {
-        url: "postgresql://neondb_owner:5kdoujSv7QbV@ep-delicate-glade-a5x3ob8y.us-east-2.aws.neon.tech/ai?sslmode=require",
+        url: process.env.DATABASE_URL,
     },
     out: "./drizzle", // Specify the output directory
 };
